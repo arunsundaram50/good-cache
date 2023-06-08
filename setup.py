bcfd342b83bdfd526881549e0d5e8256ecf7209d
Arun Sundaram <arun_co@yahoo.com>
2023-06-07 22:30:33 -0400

    dist fixes

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+  requirements = f.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+  long_description = fh.read()
 
 setup(
     name='good_cache',
-    version='0.1.3',
+    version='0.1.31',
     url='https://github.com/arunsundaram50/good-cache.git',
     author='Arun Sundaram',
     author_email='arun_co@yahoo.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     description='Efficient function output caching based on input files and other parameters to the function.',
     packages=find_packages(where="src"),  # Specifies the src directory
     package_dir={"": "src"},  # Specifies the package directory
